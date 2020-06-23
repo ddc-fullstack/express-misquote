ad5ad68008e1e40a22fb503928bb6825b0ffdd54
gkephart <gkephart@cnm.edu>
2020-06-23 17:43:26 -0600

    added a jwt route handler

--- app/src/utils/http-config.js
+++ app/src/utils/http-config.js
@@ -2,15 +2,11 @@ import axios from "axios";
 
 export const httpConfig = axios.create();
 
-httpConfig.interceptors.response.use(function ({data, headers} ) {
-  if(data.status === 200) {
-
-
-    return (
-    data.data !== null
+httpConfig.interceptors.response.use(function ({data, headers}) {
+  if (data.status === 200) {
+    return data.data !== null
       ? {message: null, data: data.data, status: 200, type: " alert alert-success", headers: {...headers}}
-      : {message: data.message, status: 200, type: "alert alert-success", data: null, headers: {...headers}}
-    )
+      : {message: data.message, status: 200, type: " alert alert-success", data: null, headers: {...headers}};
   }
   return {message: data.message, status: data.status, type: "alert alert-danger", data: null, headers: {...headers}}
 
@@ -19,3 +15,17 @@ httpConfig.interceptors.response.use(function ({data, headers} ) {
   console.log(error);
   return Promise.reject(error);
 });
+
+httpConfig.interceptors.request.use(
+  (config) => {
+    const authorization = window.localStorage.getItem("authorization")
+    if (authorization) {
+      config.headers['authorization'] = authorization
+    }
+    return config
+  }, (error) => {
+    // Do something with response error
+    console.log(error);
+    return Promise.reject(error);
+  }
+)
